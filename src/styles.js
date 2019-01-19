c47038e8e53c3760cb97f1abbd2125b97ea1fb98
everton.renan.pc@gmail.com <everton.renan.pc@gmail.com>
2019-01-19 10:28:58 -0200

    Add global stylesheet and remove build folder from repo

--- src/styles.js
+++ src/styles.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#c4b4a4'
   },
   welcome: {
     fontSize: 20,
