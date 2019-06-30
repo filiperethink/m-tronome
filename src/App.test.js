7c66b71469df0e27d32d0ce96defa5e86e9dcce4
everton.renan.pc@gmail.com <everton.renan.pc@gmail.com>
2019-06-30 12:19:11 -0300

    Add unversioned files

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from './App'
+
+it('renders correctly', () => {
+  const app = renderer.create(<App />).toJSON()
+  expect(app).toMatchSnapshot()
+})
